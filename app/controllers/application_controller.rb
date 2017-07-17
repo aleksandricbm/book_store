0d8e93c5a33772a9f6e3980c4b2fc3f7badc3880
aleksandricbm <mycompanycomel@gmail.com>
2017-07-17 11:54:45 +0300

    config, form rebuild to haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :reset_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  add_flash_types :error, :success
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |user|
+      user.permit(:email, :password, :remember_me)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:email, :current_password, :password, :password_confirmation)
+    end
+  end
+
 end
