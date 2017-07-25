02b7f0cbdb07429614440f49eeea30e4a3061e25
Aleksandr Ignatkin <mycompanycomel@gmail.com>
2017-07-26 00:36:14 +0300

    add EN locales in store

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#home'
-  get 'users/profile'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources 'book'
