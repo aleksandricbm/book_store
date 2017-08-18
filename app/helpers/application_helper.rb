59e992b3a00c71f727bbf01422e371242d2fdee2
Aleksandr Ignatkin <mycompanycomel@gmail.com>
2017-08-18 14:37:38 +0300

    fix bug catalog, added active class to the list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     templ = "<span class='shop-quantity'>#{current_order.total_quantity}</span>"
     templ.html_safe unless current_order.total_quantity < 1
   end
+
+  def active_category (category)
+    p "#{params[:id]} = #{category}"
+    (params[:id].to_i == category.to_i) ? 'active' : ''
+  end
 end
